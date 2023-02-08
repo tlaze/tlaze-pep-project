0114012ecc4f4e1ae656175aa3b8e73bf3728e46
Tom Lazore <tomlazore@gmail.com>
2023-02-08 15:55:46 +0000

    Created an addAccount method in accountDAO

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -26,5 +26,25 @@ public class AccountDAO {
         }
         return accounts;
     }
+
+    public Account addAccount(Account account){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "INSERT INTO account (account_id, username, password) VALUES (?,?,?);";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setInt(1, account.account_id);
+            preparedStatement.setString(2, account.username);
+            preparedStatement.setString(3, account.password);
+
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if(pkeyResultSet.next()){
+                int generated_account_id = (int) pkeyResultSet.getLong(1);
+                return new Account(generated_account_id, account.getUsername(), account.getPassword());
+
+            }
+        }
+    }
     
 }
